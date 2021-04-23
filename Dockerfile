4d512ebb83642c66b34d27f7c691a496e0fae93a
Kyle Bader <kbader@redhat.com>
2021-04-23 14:10:40 -0700

    add version

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openvino/model_server:latest
 
-RUN mkdir -P /tmp/models/${MODEL}/${PRECISION}/
+RUN mkdir -P /tmp/models/${VERSION}/${MODEL}/${PRECISION}/
 RUN python3 getmodel.py
--- getmodel.py
+++ getmodel.py
@@ -4,10 +4,11 @@ import requests
 zoo_base = os.environ['ZOO_BASE']
 model_name = os.environ['MODEL']
 precision = os.environ['PRECISION']
-base_path = '/tmp/models/' + model_name + '/' + precision + '/' + model_name
+version = os.environ['VERSION']
+base_path = '/tmp/models/' + version + '/' + model_name + '/' + precision + '/' + model_name
 
 for suffix in ['.bin', '.xml']:
-  url = zoo_base + '/' + model_name + '/' +  precision + '/' + model_name + suffix
+  url = zoo_base + '/' version + '/' + model_name + '/' +  precision + '/' + model_name + suffix
   response = requests.get(url)
   file = open(base_path + suffix, "wb")
   file.write(response.content)
